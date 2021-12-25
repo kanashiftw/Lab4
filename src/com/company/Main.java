d33dab925510e9be0ff8ff1070bb50d3fcbbbcad
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-12-25 23:50:41 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int key;
+        System.out.println("Введите размер массива(четное число).");
+        while(!in.hasNextInt()) {
+            System.out.println("Вводятся только числа.");
+            in.next();
+        }
+        key = in.nextInt();
+        String str = in.next();
+        System.out.println(str);
+        System.out.println(Vernam(str, key));
+    }
+    private static String Vernam(String s, int key) {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            res.append((char) (c ^ key));
+        }
+        return res.toString();
+    }
+}
