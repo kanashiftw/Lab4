0d49557e0d6295b85bd54804678e9aae8dd37bd7
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-12-26 14:53:06 +0300

    Update 5.java

--- src/com/company/5.java
+++ src/com/company/5.java
@@ -9,10 +9,26 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите строку.");
         String line = in.nextLine();
+        //С помощью регулярных выражений
+        System.out.println("С помощью регулярных выражений:");
         Pattern pattern = Pattern.compile("([A-Z]{1})([a-z]*)([0-9]{2})");
         Matcher matcher = pattern.matcher(line);
         while (matcher.find()) {
             System.out.println(line.substring(matcher.start(), matcher.end()));
+        }
+        //С помощью массива символов
+        String line2 = line.replace("-", " ");
+        line2 = line2.replace(":", " ");
+        line2 = line2.replace(",", " ");
+        String[] splitted = line2.split(" ");
+        System.out.println("С помощью массива:");
+        for(String element :splitted){
+            element = element.trim();
+            char [] mas = element.toCharArray();
+            if ((mas[0]>='A') && (mas[0]<= 'Z') && (mas[mas.length-1]>= '0') && (mas[mas.length-1]<='9') && (mas[mas.length-2]>= '0') && (mas[mas.length-2]<='9')){
+            System.out.println(mas);
+            }
+
         }
     }
 }
