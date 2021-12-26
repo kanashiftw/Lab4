3fe3040afe6e91b82831d216c063ee97e909b067
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-12-26 12:48:23 +0300

    Update 2.java

--- src/com/company/2.java
+++ src/com/company/2.java
@@ -21,20 +21,31 @@ public class Main {
             }
             break;
         }
+        //С помощью массива символов
+        char array[] = line.toCharArray();
+        for (i = 0; i < array.length; i++) {
+            if (Character.isLetter(array[i])) {
+                System.out.print(array[i]);
+            } else {
+                System.out.printf("(%d)", num);
+                System.out.print(array[i]);
+                num++;
+            }
+        }
+        System.out.println("");
         //С помощью методов String
-        for (i = 0; i < line.lastIndexOf(".")+1;i++){
+        int q = 1;
+        String linezxc = line.replace(",","(),");
+        linezxc = linezxc.replace("-","()-");
+        linezxc = linezxc.replace(":","():");
+        linezxc = linezxc.replace(".","().");
+        linezxc = linezxc.replace(" ","() ");
+        for (i = 0; i < linezxc.length();i++){
+            if (linezxc.charAt(i) == '(') {
+                linezxc = linezxc.substring(0, i+1) + (q) + linezxc.substring(i+1,linezxc.length());
+                q++;
+            }
         }
-            //С помощью массива символов
-           char array[] = line.toCharArray();
-           for (char element:array){
-               if (Character.isLetter(element)){
-               System.out.print(element);
-               }else{
-                   System.out.printf("(%d)",num);
-                   System.out.print(element);
-                   num++;
-               }
-           }
-        }
+        System.out.println(linezxc);
     }
-
+}
